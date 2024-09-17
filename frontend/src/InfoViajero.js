38174910a9420ded47cb6ca4a0f9a4ad7ccb0413
VALENTINA <valensoto486@gmail.com>
2024-09-17 11:02:44 -0500

    Version 1 pagina de inicio

---
+++ frontend/src/InfoViajero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function InfoViajero() {
+  return (
+    <div>
+      <h2>Información</h2>
+      <p>Página de información.</p>
+    </div>
+  );
+}
+
+export default InfoViajero;
---
+++ frontend/src/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Inicio() {
+  return (
+    <div>
+      <h2>Inicio</h2>
+      <p>Esta es la página de Inicio.</p>
+    </div>
+  );
+}
+
+export default Inicio;
