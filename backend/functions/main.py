a0df5f4f3cdf9d5c5631b32dc1bbb1020c53c395
Juan Felipe <juannfe2004@gmail.com>
2024-10-01 13:28:39 -0500

    Se agrego una funcion para modificar una ubicacion turistica

--- backend/functions/main.py
+++ backend/functions/main.py
@@ -14,7 +14,7 @@ from src.Autenticador.Autenticador import ProbarAuth
 from src.Storage.Storage import ProbarStorage
 
 from src.Funciones.InicioSesion import CrearUsuario, AutenticarUsuario
-from src.Funciones.Ubicaciones import CrearUbicacion
+from src.Funciones.Ubicaciones import CrearUbicacion, ModificarUbicacion
 
 
 @https_fn.on_request()
--- backend/functions/src/Funciones/Ubicaciones.py
+++ backend/functions/src/Funciones/Ubicaciones.py
@@ -66,3 +66,53 @@ def CrearUbicacion(request) -> https_fn.Response:
 
     except Exception as e:
         return https_fn.Response("Ocurrio un error creando el usuario: " + str(e))
+
+
+# [POST] Esta funcion se encargara de modificar un sitio turistico en base a las indicaciones dadas
+# Recibe un JSON con: (Nombre, UUID, Tipo, Descripcion, Latitud, Longitud)
+@https_fn.on_request()
+def ModificarUbicacion(request) -> https_fn.Response:
+    try:
+        
+        if not request.is_json:
+            return https_fn.Response('No hay JSON')
+
+        parametros = request.get_json()
+
+        uuid = parametros.get('uuid')
+        nombre = parametros.get('nombre')
+
+        # Validamos que no exista un lugar con el mismo nombre y diferente UUID
+
+        lugares_ref = firestore_db.collection("Lugares")
+
+        query_validacion = lugares_ref.where("Nombre", "==", nombre)
+        res_validacion = query_validacion.get() 
+
+        if (res_validacion and res_validacion[0].id != uuid):
+            return https_fn.Response("Ya existe una ubicacion con el nombre: " + nombre)
+        
+        # Despues de la validacion, se procede a hacer modificar el lugar
+
+        tipo = parametros.get('tipo')
+        municipio = parametros.get('municipio')
+        descripcion = parametros.get('descripcion')
+        latidud = float(parametros.get('latitud'))
+        longitud = float(parametros.get('longitud'))
+
+        informacion = {
+            "Nombre": nombre,
+            "Municipio": municipio,
+            "Tipo": tipo,
+            "Descripcion": descripcion,
+            "Latitud": latidud,
+            "Longitud": longitud
+        }
+
+        nueva_ubicacion = firestore_db.collection('Lugares').document(uuid)
+        nueva_ubicacion.update(informacion)
+
+        return https_fn.Response("Se modifico la ubicacion correctamente")
+
+    except Exception as e:
+        return https_fn.Response("Ocurrio un error creando el usuario: " + str(e))
